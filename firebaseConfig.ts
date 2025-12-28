149da6607b00a52c128b481dd8fb322bd36be603
Shaquille Rashad Mandy <shaqmandy@gmail.com>
2025-12-28 20:09:42 -0300

    adding firebaseConfig

---
+++ firebaseConfig.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyA2p3PI2Gfa_Sh4O_N1yedcdH3Eh5Me7OA',
+	authDomain: 'shaq-apps-tracker-2025.firebaseapp.com',
+	projectId: 'shaq-apps-tracker-2025',
+	storageBucket: 'shaq-apps-tracker-2025.firebasestorage.app',
+	messagingSenderId: '629762637126',
+	appId: '1:629762637126:web:a3aa9e15fe796575477947',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export default app
